ec24e8125297ea9ab9168309bb05731d592f816d
hitesh3111 <hiteshdandotiya13@gmail.com>
2022-06-27 23:03:46 +0530

    Calculator_demo code commit handle negative numbers

--- src/test/java/NormalCalculator.java
+++ src/test/java/NormalCalculator.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,84 +21,101 @@ public class NormalCalculator   {
 		
 	}
 	
-	public ArrayList<Integer> arraylist(int Number)
-	{
-		// This helper method takes a number and 
-		// Returns all of its digits in an arraylist
-		
-		int temp=Number;
-		int reminder;
-		ArrayList<Integer> al=new ArrayList<Integer>();
-		while(temp!=0)
-		{
-			reminder=temp%10;
-			al.add(reminder);
-			temp=temp/10;
-		}
-		return al;
-	}
 	
-	
-	 @Parameters({"Number1", "Number2","Operator","Expected"})
-	@Test
-	public void calculations(int Number1, int Number2,String Operator,float Expected)
-	{
-		// Using arraylist to manipulate xpath for digits in number
-		ArrayList<?> al_num1=new ArrayList<Object>();
-		ArrayList<?> al_num2=new ArrayList<Object>();
-	    
-		al_num1 = arraylist(Number1);
-		al_num2 = arraylist(Number2);
-		
-		// Entering number 1
-		for(int i=al_num1.size()-1;i>=0;i--)
-		{
-			driver.findElement(By.xpath("//span[@onclick='r("+al_num1.get(i)+")']")).click();	
-		}
-		
-		// Decision based on Operator Input 		
-		if (Operator.equals("Addition"))
-		{
-		    driver.findElement(By.xpath("//span[(text()='+')]")).click();
-		}
-		else if(Operator.equals("Subtraction"))
-		{
-			 driver.findElement(By.xpath("//span[@onclick=\"r('-')\"]")).click();
-		}
-		else if(Operator.equals("Multiplication"))
-		{
-			 driver.findElement(By.xpath("//span[@onclick=\"r('*')\"]")).click();
-		}
-		else if(Operator.equals("Division"))
-		{
-			 driver.findElement(By.xpath("//span[@onclick=\"r('/')\"]")).click();
-		}
-		else
-		{
-			System.out.println("Invalid Argument");
-		}
-		
-		// Entering number 2
-		for(int i=al_num2.size()-1;i>=0;i--)
-		{
-			driver.findElement(By.xpath("//span[@onclick='r("+al_num2.get(i)+")']")).click();	
-		}
-		
-		// Validation
-		String out;
-		out = driver.findElement(By.xpath("//div[@id='sciOutPut']")).getText().trim();
-		Float output = Float.parseFloat(out);
-		Assert.assertTrue(Expected == output);
-		
-	}
+	   public void pressNumber(WebDriver driver, int Number)
+	    {
+		  driver.findElement(By.xpath("//span[@onclick='r("+Number+")']")).click();
+	    }
 	 
+	 public void pressOperator(WebDriver driver, char Operator)
+	 {
+		 if (Operator == '+' )
+			{
+			    driver.findElement(By.xpath("//span[(text()='+')]")).click();
+			}
+			else if(Operator == '-' )
+			{
+				 driver.findElement(By.xpath("//span[@onclick=\"r('-')\"]")).click();
+			}
+			else if(Operator == '*')
+			{
+				 driver.findElement(By.xpath("//span[@onclick=\"r('*')\"]")).click();
+			}
+			else if(Operator == '/')
+			{
+				 driver.findElement(By.xpath("//span[@onclick=\"r('/')\"]")).click();
+			}
+			else
+			{
+				System.out.println("Invalid Argument");
+			}
+	 }
+	 
+	 
+	 @Parameters({"Number1", "Number2","Operator","Expected"})
+		@Test
+		public void calculations2(int Number1, int Number2,String Operator,float Expected)
+	 {
+		 String snum1= Integer.toString(Number1);
+		 String snum2= Integer.toString(Number2);
+		 
+		 for(int i=0;i<snum1.length();i++)
+		 {
+			 if(snum1.charAt(i)== '+' || snum1.charAt(i)== '-' || snum1.charAt(i)== '*' || snum1.charAt(i)== '/')
+			 {
+				 pressOperator(driver, snum1.charAt(i));
+			 }
+			 else
+			 {
+				 int numdigit= Character.getNumericValue(snum1.charAt(i));
+				 pressNumber(driver, numdigit);
+			 }
+		 }
+		 
+		 if (Operator.equals("Addition"))
+			{
+			    driver.findElement(By.xpath("//span[(text()='+')]")).click();
+			}
+			else if(Operator.equals("Subtraction"))
+			{
+				 driver.findElement(By.xpath("//span[@onclick=\"r('-')\"]")).click();
+			}
+			else if(Operator.equals("Multiplication"))
+			{
+				 driver.findElement(By.xpath("//span[@onclick=\"r('*')\"]")).click();
+			}
+			else if(Operator.equals("Division"))
+			{
+				 driver.findElement(By.xpath("//span[@onclick=\"r('/')\"]")).click();
+			}
+			else
+			{
+				System.out.println("Invalid Argument");
+			}
+		 
+		 for(int i=0;i<snum2.length();i++)
+		 {
+			 if(snum2.charAt(i)== '+' || snum2.charAt(i)== '-' || snum2.charAt(i)== '*' || snum2.charAt(i)== '/')
+			 {
+				 pressOperator(driver, snum2.charAt(i));
+			 }
+			 else
+			 {
+				 int numdigit= Character.getNumericValue(snum2.charAt(i));
+				 pressNumber(driver, numdigit);
+			 }
+		 }
+		 //Validation
+			String out;
+			out = driver.findElement(By.xpath("//div[@id='sciOutPut']")).getText().trim();
+			Float output = Float.parseFloat(out);
+			Assert.assertTrue(Expected == output);
+	 }
 
-	@AfterTest
-    public void closeBrowser() throws InterruptedException
-    {
-    	Thread.sleep(5000);
-    	//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    	driver.close();
+	     @AfterTest
+         public void closeBrowser() throws InterruptedException
+          {
+    	   Thread.sleep(5000);
+        	driver.close();
+          }
     }
-
-}
